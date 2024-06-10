48d2f3ffefab431d67efb38d79577e42ad205c2d
Shinulous <wifeofafterlife@gmail.com>
2024-06-10 13:58:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function inviteBot() {
+  // Redirect to the bot's invite link
+  window.location.href =
+    'https://discord.com/oauth2/authorize?client_id=YOUR_BOT_CLIENT_ID&permissions=8&scope=bot'
+}
